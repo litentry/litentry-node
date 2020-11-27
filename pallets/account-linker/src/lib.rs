6d2c03b43faf4d69295a119b9e7d743a49d013b8
Black3HDF <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2020-11-27 17:37:06 +0800

    Preliminary Account Linker (#6)
    * use litentry prefix, block number & account id
    
    * use dynamic prefix & add a tmp test api
    
    * add block expiration check
    
    * tmp changes
    
    * add first pallet test
    
    * use tab
    
    * take block expiration check back
    
    * prune unused imports
    
    * use const

--- pallets/account-linker/src/lib.rs
+++ pallets/account-linker/src/lib.rs
@@ -1,8 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch};
-use frame_system::ensure_signed;
+use codec::Encode;
 use sp_std::prelude::*;
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, ensure};
+use frame_system::{ensure_signed};
 
 #[cfg(test)]
 mod mock;
@@ -11,6 +12,8 @@ mod mock;
 mod tests;
 
 mod util;
+
+pub const MAX_ETH_LINKS: usize = 3;
 
 pub trait Trait: frame_system::Trait {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
@@ -32,68 +35,104 @@ decl_event!(
 );
 
 decl_error! {
-    pub enum Error for Module<T: Trait> {
-        EcdsaRecoverFailure,
-    }
+	pub enum Error for Module<T: Trait> {
+		EcdsaRecoverFailure,
+		LinkRequestExpired,
+	}
 }
 
 decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Errors must be initialized if they are used by the pallet.
-        type Error = Error<T>;
+	pub struct Module<T: Trait> for enum Call where 
+		origin: T::Origin {
+		// Errors must be initialized if they are used by the pallet.
+		type Error = Error<T>;
 
-        // Events must be initialized if they are used by the pallet.
-        fn deposit_event() = default;
+		// Events must be initialized if they are used by the pallet.
+		fn deposit_event() = default;
 
-        /// separate sig to r, s, v because runtime only support array parameter with length <= 32
-        #[weight = 1]
-        pub fn link(
-            origin,
-            account: T::AccountId,
-            index: u32,
-            block_number: u32,
-            timestamp: u32,
-            r: [u8; 32],
-            s: [u8; 32],
-            v: u8,
-        ) -> dispatch::DispatchResult {
+		/// separate sig to r, s, v because runtime only support array parameter with length <= 32
+		#[weight = 1]
+		pub fn link(
+			origin,
+			account: T::AccountId,
+			index: u32,
+			expiring_block_number: T::BlockNumber,
+			r: [u8; 32],
+			s: [u8; 32],
+			v: u8,
+		) -> dispatch::DispatchResult {
 
-            let _ = ensure_signed(origin)?;
+			let _ = ensure_signed(origin)?;
 
-            let b1 = block_number.to_be_bytes();
-            let b2 = timestamp.to_be_bytes();
-            let bytes = [b1, b2].concat();
+			let current_block_number = <frame_system::Module<T>>::block_number();
+			ensure!(expiring_block_number > current_block_number, Error::<T>::LinkRequestExpired);
 
-            // TODO: add check, add accountId, add prefix
+			let mut bytes = b"Link Litentry: ".encode();
+			let mut account_vec = account.encode(); // Warning: must be 32 bytes
+			let mut expiring_block_number_vec = expiring_block_number.encode(); // Warning: must be 4 bytes
+			
+			// let mut bytes = [0u8; 51]; // TODO: need to change this if b0 changes
+			// let b0 = b"Link Litentry: ";
+			// bytes[..15].copy_from_slice(b0);
+			// bytes[15..47].copy_from_slice(&account_vec);
+			// bytes[47..].copy_from_slice(&block_number_vec);
 
-            let hash = sp_io::hashing::keccak_256(&bytes);
+			bytes.append(&mut account_vec);
+			bytes.append(&mut expiring_block_number_vec);
 
-            let mut msg = [0u8; 32];
-            let mut sig = [0u8; 65];
+			let hash = sp_io::hashing::keccak_256(&bytes);
 
-            msg[..32].copy_from_slice(&hash[..32]);
-            sig[..32].copy_from_slice(&r[..32]);
-            sig[32..64].copy_from_slice(&s[..32]);
-            sig[64] = v;
+			let mut msg = [0u8; 32];
+			let mut sig = [0u8; 65];
+	
+			msg[..32].copy_from_slice(&hash[..32]);
+			sig[..32].copy_from_slice(&r[..32]);
+			sig[32..64].copy_from_slice(&s[..32]);
+			sig[64] = v;
 
-            let addr = util::addr_from_sig(msg, sig)
-                .map_err(|_| Error::<T>::EcdsaRecoverFailure)?;
+			let addr = util::addr_from_sig(msg, sig)
+				.map_err(|_| Error::<T>::EcdsaRecoverFailure)?;
 
-            let index = index as usize;
-            let mut addrs = Self::eth_addresses(&account);
-            if (index >= addrs.len()) && (addrs.len() != 3) { // allow linking 3 eth addresses. TODO: do not use hard code
-                addrs.push(addr);
-            } else if (index >= addrs.len()) && (addrs.len() == 3) {
-                addrs[2] = addr;
-            } else {
-                addrs[index] = addr;
-            }
+			let index = index as usize;
+			let mut addrs = Self::eth_addresses(&account);
+			if (index >= addrs.len()) && (addrs.len() != MAX_ETH_LINKS) { // allow linking 3 eth addresses. TODO: do not use hard code
+				addrs.push(addr);
+			} else if (index >= addrs.len()) && (addrs.len() == MAX_ETH_LINKS) {
+				addrs[MAX_ETH_LINKS - 1] = addr;
+			} else {
+				addrs[index] = addr;
+			}
 
-            <EthereumLink<T>>::insert(account, addrs);
+			<EthereumLink<T>>::insert(account, addrs);
 
-            Ok(())
+			Ok(())
 
-        }
+		}
 
-    }
+		#[weight = 1]
+		pub fn test(
+			origin,
+			account: T::AccountId,
+			index: u32,
+			addr: [u8; 20],
+		) -> dispatch::DispatchResult {
+		
+			let _ = ensure_signed(origin)?;
+		
+			let index = index as usize;
+			let mut addrs = Self::eth_addresses(&account);
+			if (index >= addrs.len()) && (addrs.len() != MAX_ETH_LINKS) { // allow linking 3 eth addresses. TODO: do not use hard code
+				addrs.push(addr);
+			} else if (index >= addrs.len()) && (addrs.len() == MAX_ETH_LINKS) {
+				addrs[MAX_ETH_LINKS - 1] = addr;
+			} else {
+				addrs[index] = addr;
+			}
+		
+			<EthereumLink<T>>::insert(account, addrs);
+		
+			Ok(())
+		
+		}
+	}
 }
--- pallets/account-linker/src/tests.rs
+++ pallets/account-linker/src/tests.rs
@@ -1,15 +1,56 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use crate::mock::*;
+use codec::Encode;
+use frame_support::assert_ok;
+use parity_crypto::Keccak256;
+use parity_crypto::publickey::{Random, Generator, Message, sign};
 
-// #[test]
-// fn it_works_for_default_value() {
-// 	new_test_ext().execute_with(|| {
-// 		// Dispatch a signed extrinsic.
-// 		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-// 		// Read pallet storage and assert an expected result.
-// 		assert_eq!(TemplateModule::something(), Some(42));
-// 	});
-// }
+#[test]
+fn it_works_for_default_value() {
+	new_test_ext().execute_with(|| {
+
+		let account: u64 = 5;
+		let block_number: u64 = 9999;
+
+		let mut bytes = b"Link Litentry: ".encode();
+		let mut account_vec = account.encode(); // Warning: must be 32 bytes
+		let mut expiring_block_number_vec = block_number.encode(); // Warning: must be 4 bytes
+
+		bytes.append(&mut account_vec);
+		bytes.append(&mut expiring_block_number_vec);
+
+		let msg = Message::from(bytes.keccak256());
+
+		let mut gen = Random{};
+		let key_pair = gen.generate().unwrap();
+
+		let sig = sign(key_pair.secret(), &msg).unwrap().into_electrum();
+
+		let mut r = [0u8; 32];
+		let mut s = [0u8; 32];
+
+		r[..32].copy_from_slice(&sig[..32]);
+		s[..32].copy_from_slice(&sig[32..64]);
+		let v = sig[64];
+
+		assert_ok!(AccountLinker::link(
+			Origin::signed(1),
+			account,
+			0,
+			block_number,
+			r,
+			s,
+			v
+		));
+		assert_eq!(AccountLinker::eth_addresses(account), vec![key_pair.address().to_fixed_bytes()]);
+	});
+}
+
+// account: T::AccountId,
+// index: u32,
+// block_number: T::BlockNumber,
+// r: [u8; 32],
+// s: [u8; 32],
+// v: u8,
 
 // #[test]
 // fn correct_error_for_none_value() {
