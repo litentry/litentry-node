8451384ced0c7d562eb95475cdf83d65bc452317
Han Zhao <windzhaohan@gmail.com>
2020-12-04 17:13:58 +0100

    Update readme for tests, update package json and chaijs framework

--- ts-tests/tests/test-eth-balance.ts
+++ ts-tests/tests/test-eth-balance.ts
@@ -3,12 +3,20 @@ import { KeyringPair } from '@polkadot/keyring/types';
 import { U8aFixed } from '@polkadot/types/codec';
 import * as crypto from '@polkadot/util-crypto';
 import { testValidator } from "@polkadot/util-crypto/base32/is";
+import { expect } from "chai";
 
 // Provider is set to localhost for development
 const wsProvider = new WsProvider("ws://localhost:9944");
 
 // Keyring needed to sign using Alice account
 const keyring = new Keyring({ type: 'sr25519' });
+
+// Configs of test ropsten account
+const test_eth_address = "[0x167453188a05082e3b347c1c518f3dd55d37fbbf]";
+const test_r = '0x4306864b7928f8393579ad849fd47685840cdf197c0085a5d241d28b9c6d14b3';
+const test_s = '0x3f4d2f5912213b3c0983c01a2199759b794d1423fe66cd5076b6ca9f5f60e45f';
+const test_v = 27;
+
 
 // Setup the API and Alice Account
 async function init() {
@@ -60,10 +68,6 @@ async function eth_link(api: ApiPromise, alice: KeyringPair) {
 
   console.log(`\nStep 1: Link Ethereum account`);
 
-  const test_r = '0x4306864b7928f8393579ad849fd47685840cdf197c0085a5d241d28b9c6d14b3';
-  const test_s = '0x3f4d2f5912213b3c0983c01a2199759b794d1423fe66cd5076b6ca9f5f60e45f';
-  const test_v = 27;
-
   const transaction = api.tx.accountLinkerModule.link(alice.address, 0, 0, 0, test_r, test_s, test_v);
 
   const link = new Promise<{ block: string }>(async (resolve, reject) => {
@@ -71,9 +75,12 @@ async function eth_link(api: ApiPromise, alice: KeyringPair) {
 			console.log(`Link creation is ${result.status}`);
 			if (result.status.isInBlock) {
 				console.log(`Link included at blockHash ${result.status.asInBlock}`);
+        console.log(`Waiting for finalization... (can take a minute)`);
+      } else if (result.status.isFinalized) {
+				console.log(`Transfer finalized at blockHash ${result.status.asFinalized}`);
 				unsub();
 				resolve({
-					block: result.status.asInBlock.toString(),
+					block: result.status.asFinalized.toString(),
 				});
 			}
 		});
@@ -92,7 +99,9 @@ async function check_linking_state(api: ApiPromise, alice: KeyringPair) {
 	console.log(`Alice Substrate Account (nonce: ${nonce}) balance, free: ${balance.free}`);
 
 	const linkedEthAddress = (await api.query.accountLinkerModule.ethereumLink(alice.address));
-	console.log(`Linked Ethereum addresses of Alice are: ${linkedEthAddress.toString()}`);
+  console.log(`Linked Ethereum addresses of Alice are: ${linkedEthAddress.toString()}`);
+  
+  expect(linkedEthAddress.toString()).to.equal(test_eth_address);
 
 	return;
 }
@@ -133,7 +142,10 @@ async function get_assets(api: ApiPromise, alice: KeyringPair) {
 	console.log(`Alice Substrate Account (nonce: ${nonce}) balance, free: ${balance.free}`);
 
 	const assetsBalances = (await api.query.offchainWorkerModule.accountBalance(alice.address));
-	console.log(`Linked Ethereum balances of Alice are: ${assetsBalances.toString()}`);
+  console.log(`Linked Ethereum balances of Alice are: ${assetsBalances.toString()}`);
+  
+  // TODO fetch real time balance and compare it here
+  expect(assetsBalances.toString()).to.equal(`[0,"0x000000000000000006f05b59d3b20000"]`);
 
 	return;
 
