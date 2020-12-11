bced539110a9b794879285031133c369c15b801e
Satoshi-Kusumoto <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2020-12-11 12:06:02 +0800

    add p2pkh pk to addr & a test

--- pallets/account-linker/src/util_btc.rs
+++ pallets/account-linker/src/util_btc.rs
@@ -1,8 +1,28 @@
 use sha2::{Digest, Sha256};
 use ripemd160::Ripemd160;
 
+// pub fn btc_addr_from_pk_compressed(pk: [u8; 33]) {
 
-pub fn hash160(bytes: &[u8]) -> [u8; 20] {
+// }
+
+pub fn btc_addr_from_pk_uncompressed(pk: [u8; 65]) -> [u8; 25] {
+    let mut result = [0u8; 25];
+
+    // Now only support P2PKH (Mainnet)
+    result[0] = 0;
+    result[1..21].copy_from_slice(&hash160(&pk));
+    let cs = checksum(&result[0..21]);
+    result[21..25].copy_from_slice(&cs);
+    result
+}
+
+fn checksum(input: &[u8]) -> [u8; 4] {
+	let mut result = [0u8; 4];
+	result.copy_from_slice(&dsha256(input)[0..4]);
+	result
+}
+
+fn hash160(bytes: &[u8]) -> [u8; 20] {
     let mut hasher_sha256 = Sha256::new();
     hasher_sha256.update(bytes);
     let digest = hasher_sha256.finalize();
@@ -22,7 +42,7 @@ pub fn hash160(bytes: &[u8]) -> [u8; 20] {
 ///
 /// # Returns
 /// * The double SHA256 hash encoded as LE bytes from data
-pub fn dsha256(bytes: &[u8]) -> [u8; 32] {
+fn dsha256(bytes: &[u8]) -> [u8; 32] {
     let mut hasher = Sha256::new();
     hasher.update(bytes);
     let digest = hasher.finalize();
@@ -53,6 +73,21 @@ mod tests {
 		hash_expected[0..20].copy_from_slice(&result[0..20]);
 
 		assert_eq!(hash, hash_expected);
-	}
+    }
+    
+    #[test]
+    fn correct_btc_addr_from_pk_uncompressed() {
+        let pk = decode("0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6").unwrap();
+        let mut pk_input = [0u8; 65];
+        pk_input[0..65].copy_from_slice(&pk[0..65]);
+
+        let addr = btc_addr_from_pk_uncompressed(pk_input);
+
+        let addr_expected_hex = decode("00010966776006953D5567439E5E39F86A0D273BEED61967F6").unwrap();
+        let mut addr_expected = [0u8; 25];
+        addr_expected[0..25].copy_from_slice(&addr_expected_hex[0..25]);
+        assert_eq!(addr, addr_expected);
+
+    }
 
 }
