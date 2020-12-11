1caa65335f38ce8be0fedf5927e54e4265c4fd3e
Satoshi-Kusumoto <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2020-12-11 11:33:33 +0800

    add dhash160 and a test

--- pallets/account-linker/src/lib.rs
+++ pallets/account-linker/src/lib.rs
@@ -2,6 +2,7 @@
 
 use codec::Encode;
 use sp_std::prelude::*;
+// use sp_io::secp256k1_ecdsa_recover_compressed;
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, ensure};
 use frame_system::{ensure_signed};
 
@@ -12,6 +13,7 @@ mod mock;
 mod tests;
 
 mod util;
+mod util_btc;
 
 pub const MAX_ETH_LINKS: usize = 3;
 pub const MAX_BTC_LINKS: usize = 3;
@@ -23,6 +25,7 @@ pub trait Trait: frame_system::Trait {
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
 		pub EthereumLink get(fn eth_addresses): map hasher(blake2_128_concat) T::AccountId => Vec<[u8; 20]>;
+		pub BitcoinLink get(fn btc_addresses): map hasher(blake2_128_concat) T::AccountId => Vec<[u8; 20]>;
 	}
 }
 
@@ -104,6 +107,62 @@ decl_module! {
 			Ok(())
 
 		}
+
+		// /// separate sig to r, s, v because runtime only support array parameter with length <= 32
+		// #[weight = 1]
+		// pub fn link_btc(
+		// 	origin,
+		// 	account: T::AccountId,
+		// 	index: u32,
+		// 	expiring_block_number: T::BlockNumber,
+		// 	r: [u8; 32],
+		// 	s: [u8; 32],
+		// 	v: u8,
+		// ) -> dispatch::DispatchResult {
+
+		// 	let _ = ensure_signed(origin)?;
+
+		// 	let current_block_number = <frame_system::Module<T>>::block_number();
+		// 	ensure!(expiring_block_number > current_block_number, Error::<T>::LinkRequestExpired);
+
+		// 	let mut bytes = b"Link Litentry: ".encode();
+		// 	let mut account_vec = account.encode();
+		// 	let mut expiring_block_number_vec = expiring_block_number.encode();
+
+		// 	bytes.append(&mut account_vec);
+		// 	bytes.append(&mut expiring_block_number_vec);
+
+		// 	let hash = sp_io::hashing::keccak_256(&bytes);
+
+		// 	let mut msg = [0u8; 32];
+		// 	let mut sig = [0u8; 65];
+
+		// 	msg[..32].copy_from_slice(&hash[..32]);
+		// 	sig[..32].copy_from_slice(&r[..32]);
+		// 	sig[32..64].copy_from_slice(&s[..32]);
+		// 	sig[64] = v;
+
+		// 	let pk = secp256k1_ecdsa_recover_compressed(msg, sig)
+		// 		.map_err(|_| Error::<T>::EcdsaRecoverFailure)?;
+
+		// 	let addr = util::btc_addr_from_pk(pk);
+
+		// 	let index = index as usize;
+		// 	let mut addrs = Self::eth_addresses(&account);
+		// 	// NOTE: allow linking `MAX_ETH_LINKS` eth addresses.
+		// 	if (index >= addrs.len()) && (addrs.len() != MAX_ETH_LINKS) {
+		// 		addrs.push(addr);
+		// 	} else if (index >= addrs.len()) && (addrs.len() == MAX_ETH_LINKS) {
+		// 		addrs[MAX_ETH_LINKS - 1] = addr;
+		// 	} else {
+		// 		addrs[index] = addr;
+		// 	}
+
+		// 	<EthereumLink<T>>::insert(account, addrs);
+
+		// 	Ok(())
+
+		// }
 
 		#[weight = 1]
 		pub fn test(
